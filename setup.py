0721a315b45e834e58db7e139ba774646d24941a
Nick Humrich <nick.humrich@canopytax.com>
2019-08-01 10:26:57 -0600

    Update version

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ requires = [
     'aioauth-client',
     'cryptography',
     'aiohttp>=3.4.0',
-    'aiohttp_session>=2.7.0'
+    'aiohttp_session>=2.7.0',
     'pyJWT'
 ]
 
 setup_options = dict(
     name='aiohttp_oauth',
-    version='0.9.1',
+    version='0.9.3',
     description='oauth middleware for aiohttp',
     long_description='See readme at '
                      'https://github.com/CanopyTax/aiohttp-oauth',
